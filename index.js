606599a94f9b0be74ba6ebabfc949f251535f909
saikanal <srikanal@gmail.com>
2019-09-24 16:08:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('hello kannan manoj')
+  response.send('Welcome manoj')
 })
 
 app.listen(app.get('port'), function() {
